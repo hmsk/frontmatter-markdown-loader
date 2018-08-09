49edea3c455d24751a077b1f863c004041dab342
hmsk <k.hamasaki@gmail.com>
2018-08-09 00:01:18 -0700

    Test base behaviors

--- test/frontmatter-markdown-loader.test.js
+++ test/frontmatter-markdown-loader.test.js
@@ -9,8 +9,36 @@ const load = (source) => {
   return JSON.parse(rawLoaded.replace(/^module.exports =/, ""));
 }
 
+const markdownWithFrontmatter = `---
+subject: Hello
+tags:
+  - tag1
+  - tag2
+---
+# Title
+
+GOOD BYE
+`;
+
 describe("loader", () => {
+  let loaded;
+
+  beforeEach(() => {
+    loaded = load(markdownWithFrontmatter);
+  });
+
   it("returns compiled HTML for 'html' property", () => {
-    expect(load("# Title").html).toBe("<h1>Title</h1>\n");
+    expect(loaded.html).toBe("<h1>Title</h1>\n<p>GOOD BYE</p>\n");
+  });
+
+  it("returns raw markdown body for 'body' property", () => {
+    expect(loaded.body).toBe("# Title\n\nGOOD BYE\n");
+  });
+
+ it("returns frontmatter object for 'attributes' property", () => {
+    expect(loaded.attributes).toEqual({
+      subject: "Hello",
+      tags: ["tag1", "tag2"]
+    });
   });
 });
