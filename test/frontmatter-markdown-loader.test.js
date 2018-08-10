1d34598339acb9e73adaab6f399d93c4d816c1f6
hmsk <k.hamasaki@gmail.com>
2018-08-10 02:06:02 -0700

    Test vue option too

--- test/frontmatter-markdown-loader.test.js
+++ test/frontmatter-markdown-loader.test.js
@@ -1,10 +1,10 @@
 import Loader from "../index";
 
-const context = {
+const defaultContext = {
   cachable: false
 };
 
-const load = (source) => {
+const load = (source, context = defaultContext) => {
   const rawLoaded = Loader.call(context, source);
   return JSON.parse(rawLoaded.replace(/^module.exports =/, ""));
 }
@@ -20,25 +20,44 @@ tags:
 GOOD BYE
 `;
 
-describe("loader", () => {
+describe("frontmatter-markdown-loader", () => {
   let loaded;
 
-  beforeEach(() => {
-    loaded = load(markdownWithFrontmatter);
+  describe("against Frontmatter markdown without any option", () => {
+    beforeEach(() => {
+      loaded = load(markdownWithFrontmatter);
+    });
+
+    it("returns compiled HTML for 'html' property", () => {
+      expect(loaded.html).toBe("<h1>Title</h1>\n<p>GOOD BYE</p>\n");
+    });
+
+    it("returns raw markdown body for 'body' property", () => {
+      expect(loaded.body).toBe("# Title\n\nGOOD BYE\n");
+    });
+
+  it("returns frontmatter object for 'attributes' property", () => {
+      expect(loaded.attributes).toEqual({
+        subject: "Hello",
+        tags: ["tag1", "tag2"]
+      });
+    });
   });
 
-  it("returns compiled HTML for 'html' property", () => {
-    expect(loaded.html).toBe("<h1>Title</h1>\n<p>GOOD BYE</p>\n");
+  it("doesn't returns 'vue' property", () => {
+    expect(loaded.vue).toBeUndefined();
   });
 
-  it("returns raw markdown body for 'body' property", () => {
-    expect(loaded.body).toBe("# Title\n\nGOOD BYE\n");
-  });
 
- it("returns frontmatter object for 'attributes' property", () => {
-    expect(loaded.attributes).toEqual({
-      subject: "Hello",
-      tags: ["tag1", "tag2"]
+  describe("with Vue option", () => {
+    beforeEach(() => {
+      loaded = load(markdownWithFrontmatter, { ...defaultContext, query: { vue: true } });
+    });
+
+    it("returns 'vue' property which has render and staticRenderFns", () => {
+      expect(loaded.vue).toBeDefined();
+      expect(loaded.vue.render).toBeDefined();
+      expect(loaded.vue.staticRenderFns).toBeDefined();
     });
   });
 });
